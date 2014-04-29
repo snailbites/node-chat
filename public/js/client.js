e18f7ebb837dee37169df1ffaedf79ec8b755cd5
Vincent Nalupta <Vincent.Nalupta@JCrew.Com>
2014-04-29 09:56:49 -0400

    initial commit

---
+++ public/js/client.js
@@ -0,0 +1,84 @@
+var socket = io.connect();
+
+// helper funcs
+function displayMsg(msg, pseudo){
+    $('#chatEntries').find('.last').removeClass('last');
+    $('#chatEntries').append(
+        '<div class="message last"><span class="pseudo">'
+        + pseudo
+        + '</span>: <span class="msg">' + msg + '</span></div>'
+    );
+    $("#chatEntries").scrollTop($("#chatEntries")[0].scrollHeight);
+}
+
+function sendMsg() {
+    if( $('#messageInput').val() !== ''){
+        socket.emit('message', $('#messageInput').val());
+        displayMsg($('#messageInput').val(), 'Me');
+        $('#messageInput').val('');
+    }
+}
+
+function playSound(){
+    var sound = new Audio('/sounds/beep.mp3'); // buffers automatically when created
+    sound.play();
+}
+
+function translateToLinks(msg){
+    // return msg with link
+}
+
+// set up events
+socket.on('connect', function() {
+    console.log("Welcome to Vince's chat server!");
+});
+socket.on('welcome', function(data){
+    console.log('welcome')
+    $('#chatEntries').append('<div class="welcome message"><em>' + data['pseudo'] + ' has entered the room.</em></div>');
+    $('#userlist').append('<p class="' + data['pseudo'] + '"">' + data['pseudo'] + '</p>');
+});
+socket.on('disconnect', function(data){
+    console.log('disconnect')
+    $('#chatEntries').append('<div class="welcome message"><em>' + data['pseudo'] + ' has left the room.</em></div>');
+    $('.' + data['pseudo']).remove();
+});
+
+socket.on('message', function(data){
+    displayMsg(data['message'], data['pseudo']);
+    playSound();
+
+    $(window).focus(function(){
+        window.clearInterval(intervalID);
+        document.title = "Team Central Chat";
+    });
+
+    var $pseudo = $('#chatEntries').find('.last').find('.pseudo').text(),
+        $msg = $('#chatEntries').find('.last').find('.msg').text(),
+        docTitle = false,
+        intervalID = setInterval(function(){
+        if(!docTitle){
+            docTitle = true;
+            document.title = $pseudo + " - " + $msg;
+        }
+        else {
+            docTitle = false;
+            document.title = "Team Central Chat";
+        }
+    }, 1000);
+});
+
+// attach events
+$(function(){
+    $('#submit').click(function(){
+        sendMsg();
+    });
+    $('input').bind('keyup',function(e){
+        if(e.which === 13){
+            $('button').click();
+        }
+    });
+    $('#beep').click(function(e){
+        e.preventDefault();
+
+    })
+});
---
+++ server.js
@@ -0,0 +1,62 @@
+// loading modules
+var express = require('express'),
+    ejs = require('ejs'),
+    app = express();
+
+// setting up app server
+var http = require('http'),
+    server = http.createServer(app),
+    fs    = require('fs'),
+    nconf = require('nconf'),
+    io = require('socket.io').listen(server, { log: false });
+
+// global vars
+var userct = 0;
+
+// set up views
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.set('view options', { layout: false });
+app.configure(function(){ app.use(express.static(__dirname + '/public')); });
+app.get('/', function(req,res){ res.render('index.ejs'); });
+
+// start listening on port
+server.listen(3000, "0.0.0.0");
+
+// set up events
+io.sockets.on('connection', function(socket){
+    userct++;
+    console.log(socket.handshake.address.address + ' has entered the room');
+    socket.broadcast.emit('welcome', { pseudo : returnPseudo(socket) });
+
+    socket.on('message', function(data){
+        var transmit = { date : new Date().toISOString(), pseudo : returnPseudo(socket), message : data };
+        socket.broadcast.emit('message', transmit);
+    });
+
+    socket.on('disconnect', function(){
+        socket.broadcast.emit('disconnect', { pseudo : returnPseudo(socket) });
+        console.log(socket.handshake.address.address + ' has left the room');
+    });
+});
+
+// set up user nconf
+nconf.use('file', { file: './config/users.json'});
+nconf.use('file', { file: './config/online.json'});
+nconf.load();
+
+// helper funcs
+// Return the name of the user
+function returnPseudo(socket) {
+    var pseudo,
+        address = getAddr(socket),
+        name = nconf.get(address);
+
+    return name || 'Anonymous' + userct ;
+}
+function getAddr(socket){
+    return socket.handshake.address.address;
+}
+function listUsers(socket){
+
+}
