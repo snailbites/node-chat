46249a59afa4a312fd9d6f67f9ab486b253c3b60
Jared McDonald <jared.r.mcdonald@gmail.com>
2014-11-19 12:50:28 -0500

    add files I forgot to add with last commit

---
+++ modules/sanitize-html.js
@@ -0,0 +1,6 @@
+module.exports = function sanitizeHtml (string) {
+  return string.replace(/</g, '&lt;')
+               .replace(/>/g, '&gt;')
+               .replace(/"/g, '&quot;')
+               .replace(/'/g, '&#39;')
+}
---
+++ start-dev.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SERVER_LOG="server.log"
+GRUNT_LOG="grunt.log"
+NEWLINE=""
+ARROW="===>"
+
+echo $NEWLINE
+echo "Starting server with supervisor and sending output to $SERVER_LOG..."
+echo "$ARROW nohup supervisor -i public,client server > $SERVER_LOG &"
+nohup supervisor -i public,client server > $SERVER_LOG &
+
+echo $NEWLINE
+echo "Watching public source files for changes and sending putput to $GRUNT_LOG..."
+echo "$ARROW nohup grunt watch > $GRUNT_LOG &"
+nohup grunt watch > grunt.log &
+
+echo $NEWLINE
+echo "Tailing $SERVER_LOG and $GRUNT_LOG..."
+echo "$ARROW tail -f $SERVER_LOG $GRUNT_LOG"
+echo "(exit: ^C)"
+echo $NEWLINE
+tail -f $SERVER_LOG $GRUNT_LOG
+
